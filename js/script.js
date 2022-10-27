f9176b747f269f979f4b4fb067f3ea1b7e3cc96b
Kenta Bilotta <kentabilotta20@gmail.com>
2022-10-27 16:31:14 +0200

    quarto commit: piccola correzione del ciclo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let item;
-let question;
 let lista = document.querySelector(".lista");
 alert("In questo programma potrai comporre la tua lista della spesa");
 
+let item = prompt('Inserisci il nome del prodotto. Scrivi "fine" quando vuoi concludere');
+
 while (item !== "fine") {
-    item = prompt('Inserisci il nome del prodotto. Scrivi "fine" quando vuoi concludere');
     lista.innerHTML += `<li>${item}</li>`;
     console.log(item);
+    item = prompt('Inserisci il nome del prodotto. Scrivi "fine" quando vuoi concludere');
 }
 
 
